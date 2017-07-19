d90088e9113c04006f835d6efc0d20f60237542d
Serkan Simsir <serkan@simsir.me>
2017-07-19 12:37:35 -0600

    with jquery

--- assets/main.js
+++ assets/main.js
@@ -4,25 +4,60 @@ $(function() {
     url: 'https://www.codeschool.com/users/Dr4g0n3y3.json',
     dataType: 'jsonp',
     success: function(response) {
-      for ( var i = 0; i < response.courses.completed.length; i++ ){
-        var el = document.createElement('div');
-        var img = document.createElement('img');
-        var link = document.createElement('a');
-
-        link.className = 'btn btn-primary';
-        link.setAttribute('href', response.courses.completed[i].url);
-        link.setAttribute('target', '_blank');
-        link.innerHTML = 'See Course'
-        img.setAttribute('src', response.courses.completed[i].badge);
-
-        el.className = 'course';
-        el.innerHTML = '<h3>' + response.courses.completed[i].title + '</h3>';
-        $('#badges').append(el);
-        el.append(img);
-        el.append(link);
-        console.log(innerHTML = response.courses.completed[i]);
-      }
+      var courses = response.courses.completed;
+      addCourses(courses);
     }
   });
 
+
+
+
+  function addCourses(courses) {
+
+    var $badges = $('#badges');
+
+    courses.forEach(function(course) {
+
+      var $course = $('<div />', {
+        'class': 'course'
+      }).appendTo($badges);
+
+      $('<h3 />', {
+        text: course.title
+      }).appendTo($course);
+
+      $('<img />', {
+        src: course.badge
+      }).appendTo($course);
+
+      $('<a />', {
+        href: course.url,
+        'class': 'btn btn-primary',
+        text: 'See Course',
+        target: '_blank'
+      }).appendTo($course);
+
+
+    })
+  }
+// function addCourses(courses){
+//   for ( var i = 0; i < courses.length; i++ ){
+//     var el = document.createElement('div');
+//     var img = document.createElement('img');
+//     var link = document.createElement('a');
+//
+//     link.className = 'btn btn-primary';
+//     link.setAttribute('href', courses[i].url);
+//     link.setAttribute('target', '_blank');
+//     link.innerHTML = 'See Course'
+//     img.setAttribute('src', courses[i].badge);
+//
+//     el.className = 'course';
+//     el.innerHTML = '<h3>' + courses[i].title + '</h3>';
+//     $('#badges').append(el);
+//     el.append(img);
+//     el.append(link);
+//     console.log(innerHTML = courses[i]);
+//   };
+// }
 });
